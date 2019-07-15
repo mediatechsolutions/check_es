47861da7c28fa37c85ce5afefdb9b71290d33203
Miguel Angel Garcia <miguel.garcia@mediatechsolutions.es>
2019-07-15 11:06:56 +0200

    Add check for nginx response times

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ Elasticsearch checker for nagios and icinga
 
 Dependencies
 -------------
-* pip install PySock urllib3 elasticsearch
+* pip install PySock urllib3 elasticsearch requests
+
+`nginx-response-times.py` requires python 3 and requests
 
 ----------
 
@@ -16,9 +18,11 @@ Functionalities
 
 * Execute a elasticsearch query and parse result
 * Check status cluster health
+* Check for nginx request times ranges.
 
 
 Examples
 -------------
 
 * python check_es.py --host 127.0.0.1 --port 9200 --index winlogbeat-* --fields-to-be-returned message,computer_name,source_name --critical 1 --query $QUERY
+* python nginx-response-times.py --uri http://localhost:9200 --username WHOEVER --password WHATEVER --index nginx --range 0:0.3  0.3:0.5  0.5:1:10  1:2 2:3:10:15 3: --minutes 5;
---
+++ nginx-response-times.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+import requests
+import time
+import math
+
+
+class Range:
+    def __init__(self, low, high, warning=None, critical=None):
+        self.low = low
+        self.high = high
+        self.warning = warning
+        self.critical = critical
+
+    def __str__(self):
+        return f"{self.low};{self.high};{self.warning};{self.critical}"
+
+    def __repr__(self):
+        return str(self)
+
+    @property
+    def has_alerts(self):
+        return self.warning is not None or self.critical is not None
+
+
+def split_ranges(range_list):
+    for data in range_list:
+        array = data.split(':')
+
+        low = float(array[0])
+        high = float(array[1]) if len(array) > 1 and array[1] else None
+        warn = float(array[2]) if len(array) > 2 and array[2] else None
+        crit = float(array[3]) if len(array) > 3 and array[3] else None
+
+        yield Range(low=low, high=high, warning=warn, critical=crit)
+
+    
+
+def query(args, range_list):
+    url = f"{args.uri}/{args.index}*/_search"
+    auth = (args.username, args.password)
+    ranges = [{ "from": x.low, "to": x.high} for x in range_list]
+    date_to = int(time.time() * 1000)
+    date_from = date_to - (args.minutes * 60000)
+
+    body = {
+      "aggs": {
+        "2": {
+          "range": {
+            "field": args.request_time_field,
+            "ranges": ranges,
+            "keyed": False
+          }
+        }
+      },
+      "size": 0,
+      "query": {
+        "bool": {
+          "must": [
+            {
+              "range": {
+                "@timestamp": {
+                  "gte": date_from,
+                  "lte": date_to,
+                  "format": "epoch_millis"
+                }
+              }
+            }
+          ]
+        }
+      }
+    }
+
+    r = requests.post(url, auth=auth, json=body)
+    r.raise_for_status()
+
+    return r.json()
+
+
+def print_nagios_report(range_list, result):
+    OK, WARN, CRIT = 0, 1, 2
+    buckets = result['aggregations']['2']['buckets']
+    result = OK
+    stdout = ""
+    perf = ""
+
+    def find(r):
+        for b in buckets:
+            if b.get('from') == r.low and b.get('to') == r.high:
+                return b
+
+    def check_status(r, value):
+        if r.critical is not None and value > r.critical:
+            return "CRITICAL: %s > %s" % (value, r.critical), CRIT
+        if r.warning is not None and value > r.warning:
+            return "WARNING: %s > %s" % (value, r.warning), WARN
+        return '', OK
+
+    for r in range_list:
+        bucket = find(r)
+        error, new_status = check_status(r, bucket['doc_count']) 
+        perf += "'{l}'={v};{w};{c};0; ".format(
+            l=bucket['key'],
+            v=bucket['doc_count'],
+            w='' if r.warning is None else r.warning,
+            c='' if r.critical is None else r.critical,
+        )
+        stdout += "{l} = {v} {e}\n".format(
+            l=bucket['key'],
+            v=bucket['doc_count'],
+            e=error,
+        )
+        result = max(result, new_status)
+    print("%s|%s" % (stdout, perf))
+    return result 
+        
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument(
+        '--uri', 
+        help='Host to connect to.'
+    )
+    parser.add_argument(
+        '--index', 
+        help='Index to be used'
+    )
+    parser.add_argument(
+        '--username',
+        help='User to be used on connection'
+    )
+    parser.add_argument(
+        '--password',
+        help='Password to be used on connection'
+    )
+    parser.add_argument(
+        '--minutes',
+        type=int,
+        default=5,
+        help='Search for the last N minutes'
+    )
+    parser.add_argument(
+        '--range',
+        nargs='*',
+        type=str,
+        help='Tuples (value, min, max) to generate ranges. The first one must be 0.'
+    ) "request_time"
+    parser.add_argument(
+        '--request-time-field',
+        default="request_time"
+        help='Field name where the request time is stored'
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    range_list = list(split_ranges(args.range or []))
+    result = query(args, range_list)
+    sys.exit(print_nagios_report(range_list, result))
+    
+
+if __name__ == "__main__":
+    main()
